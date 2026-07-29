e5959f9d782224bedbba0f034c78726107b9a1f8
NoemiKyoko <noemitabani90@gmail.com>
2026-07-29 11:24:34 +0200

    Update dialogo.js

--- dialogo.js
+++ dialogo.js
@@ -1,4 +1,54 @@
 /* ========================================
    DIALOGO DI BAOZZI
-   Desideri e coccole
+   Capitolo "Desideri e coccole"
    ======================================== */
+
+
+/* ========================================
+   STATI DELLA CONVERSAZIONE
+   ======================================== */
+
+const STATI_DIALOGO = {
+
+    PANCHINA: "panchina",
+
+    DESIDERIO: "desiderio",
+
+    PERMESSO: "permesso",
+
+    DOMANDA_1: "domanda1",
+
+    DOMANDA_2: "domanda2",
+
+    ESITO_SERENO: "sereno",
+
+    ESITO_ATTESA: "attesa"
+
+};
+
+
+/* ========================================
+   STATO ATTUALE
+   ======================================== */
+
+let statoDialogo =
+    STATI_DIALOGO.PANCHINA;
+
+
+/* ========================================
+   DESIDERIO SCRITTO DALL'UTENTE
+   ======================================== */
+
+let desiderio =
+    "";
+
+
+/* ========================================
+   RISPOSTE DELLE DOMANDE
+   ======================================== */
+
+let rispostaDomanda1 =
+    null;
+
+let rispostaDomanda2 =
+    null;
